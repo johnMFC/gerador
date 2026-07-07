1bca6c9e185dcb0b936616e221bcc7abf6f36f56
Johnatas michaell <johnatasmichael067@gmail.com>
2026-07-07 20:05:58 -0300

    finish_project

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getCharTypes() {
   const specialCharacters = document.getElementById("include_special_charactere").checked;
   const charTypes = [];
    
-  if (upperCase) {
+if (upperCase) {
     charTypes.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 }
 if(lowerCase) {
@@ -29,11 +29,52 @@ function randownCharType(charTypes){
 
 function getPasswordSize(){
   const size = document.querySelector("#number").value;
+  if (isNaN(size) ||  size < 4 || size > 100 ){
+     message("Por favor, insira um número entre 4 e 128!", '#dc2626');
+  }
   return size;
 }
 
+function generatePassword(size, charTypes){
+  let passwordGenerated = "";
+  while(passwordGenerated.length < size ){
+     passwordGenerated += randownCharType(charTypes);
+  }
+  return passwordGenerated;
+}
+
+function message(text,background){
+   Toastify({
+       text: text,
+       duration: 3000,
+       style :{
+           background : background,
+           boxShadow: 'none',
+       }
+     }).showToast();
+}
+
+
   document.querySelector("#generate").addEventListener("click", function() {
-     //console.log(randownCharType(getCharTypes()));
-     console.log(getPasswordSize());
+     const size = getPasswordSize();
+     const charTypes = getCharTypes();
+
+     
+      if(!size){
+        return;
+      }
+     if(!charTypes.length){
+        message("Por favor, selecione pelo menos um tipo de caractere!", '#dc2626');
+        return;
+     }
+     const passwordGenerated = generatePassword(size, charTypes);
+     
+     document.querySelector("#password_container").classList.add("show");
+     document.querySelector("#password").textContent = passwordGenerated;
+  });
+
+  document.querySelector("#copy").addEventListener("click", function(){
+    navigator.clipboard.writeText(document.querySelector("#password").textContent);
+    message("Senha copiada com sucesso!", '#84cc16');
   });
 
